348dbb6afd62e08bf55ea39bcfa61e022b1ef315
Jonas Grimfelt <grimen@gmail.com>
2009-04-10 12:18:46 +0200

    Added methods for unsafe update fo protected model attributes, i.e. admin.

--- lib/r_extensions/active_record/base.rb
+++ lib/r_extensions/active_record/base.rb
@@ -10,8 +10,27 @@ module RExtensions
       
       module ClassMethods
         
+        # Make helpers accessible from models in same way as controllers
         def helpers
           ActionController::Base.helpers
+        end
+        
+        # Build and create records unsafely, bypassing attr_accessible.
+        
+        def unsafe_build(attrs)
+          record = new
+          record.unsafe_attributes = attrs
+          record
+        end
+        
+        def unsafe_create(attrs)
+          record = unsafe_build(attrs)
+          record.save
+          record
+        end
+        
+        def unsafe_create!(attrs)
+          unsafe_build(attrs).save!
         end
         
         # Basic idea: http://github.com/kalasjocke/find_or_create_by
@@ -33,6 +52,22 @@ module RExtensions
       end
       
       module InstanceMethods
+        
+        def unsafe_update_attributes(attrs)
+          self.unsafe_attributes = attrs
+          self.save
+        end
+        
+        def unsafe_update_attributes!(attrs)
+          self.unsafe_attributes = attrs
+          self.save!
+        end
+        
+        def unsafe_attributes=(attrs)
+          attrs.each do |k, v|
+            send("#{k}=", v)
+          end
+        end
         
         def permalink
           if self.name.present?
